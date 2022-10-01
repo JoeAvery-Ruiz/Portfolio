6506203c5e7f62da6466f2490af0508a8b25478d
joeavery-ruiz <joeavery96@yahoo.com>
2022-09-30 21:13:37 -0500

    portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+    <h1>
+      Portfolio
+    </h1>
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
